11308792b8066463f9ace10be10611e8d0edb73d
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-30 17:11:12 -0300

    feat: expose vertical slice modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+export { Engine } from './core/Engine.js';
 export { EventBus } from './core/EventBus.js';
 export { createGameState, cloneGameState } from './core/GameState.js';
 export { TurnManager } from './core/TurnManager.js';
+export { COMMAND_TYPES, moveCommand, waitCommand } from './core/Command.js';
 export { Grid } from './world/Grid.js';
-
+export { DIRECTIONS, position, addPosition, samePosition } from './world/Position.js';
+export { MovementSystem } from './systems/MovementSystem.js';
