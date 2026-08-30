b8c689a100d032005b66bf8acb9a817ca23d6268
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-30 18:44:04 -0500

    feat: export occupancy manager

--- src/index.js
+++ src/index.js
@@ -5,4 +5,5 @@ export { TurnManager } from './core/TurnManager.js';
 export { COMMAND_TYPES, moveCommand, waitCommand } from './core/Command.js';
 export { Grid } from './world/Grid.js';
 export { DIRECTIONS, position, addPosition, samePosition } from './world/Position.js';
+export { EntityManager } from './world/EntityManager.js';
 export { MovementSystem } from './systems/MovementSystem.js';
