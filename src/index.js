8a5effe5e5334086fbaf384fedfa5448c49c0e4f
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-30 14:45:42 -0500

    feat: export initial engine modules

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { EventBus } from './core/EventBus.js';
+export { createGameState, cloneGameState } from './core/GameState.js';
+export { TurnManager } from './core/TurnManager.js';
+export { Grid } from './world/Grid.js';
+
